c3eb028be7d24a2907d9490ad4b6a1080fc77af9
Tanzir Hasan <63520213+TanzHasan@users.noreply.github.com>
2023-10-10 14:49:52 -0700

    Cleaned up auto-iwyu.py
    This now handles error cases and can be tested on larger batches of code.

--- auto-iwyu.py
+++ auto-iwyu.py
@@ -2,22 +2,48 @@ import json
 import subprocess
 import os
 import sys
+import shutil
 
 def main(commands, specific, fixer_path):
     with open (commands) as file:
         data = json.load(file)
         for part in data:
             if part["file"][-len(specific):] != specific: continue
-            command = part['command'].split()
-            nname = command[-2][:-1] + 'i' # o to i file extension
-            os.system("wc -l ./" + nname)
-            include = ["include-what-you-use", "-Xiwyu", "--no_default_mappings", "-Xiwyu","--max_line_length=30", "-Xiwyu", "--mapping_file=./filter.imp"] #"-Xiwyu", "--verbose=7"
-            include_command = include + command[1:] + [f'2>&1 | {fixer_path}']
-            os.system(' '.join(include_command))
-            command[-2] = nname
-            build_command = command + ['-E']
-            os.system(' '.join(build_command))
-            os.system("wc -l ./" + nname)
+            perform_iwyu(fixer_path, part)
+
+def perform_iwyu(fixer_path, part):
+    command = part['command'].split()
+    orig = command[-2]
+    nname = orig[:-1] + 'i'
+    backup_file_path = './' + orig[:-2] + '_backup.c'
+    shutil.copy('./'+orig, backup_file_path)
+    subprocess.run(["wc", "-l", f"./{nname}"])
+
+    include = [
+        "include-what-you-use",
+        "-Xiwyu",
+        "--no_default_mappings",
+        "-Xiwyu",
+        "--max_line_length=30",
+        "-Xiwyu",
+        "--mapping_file=./filter.imp"
+    ]
+    include_command = include + command[1:] + [f'2>&1 | {fixer_path}']
+    # print(include_command)
+    os.system(' '.join(include_command))
+
+    command[-2] = nname
+    build_command = command + ['-E']
+    
+
+    try:
+        subprocess.run(' '.join(build_command), shell=True)
+        subprocess.run(f"wc -l ./{nname} >> changes.txt", shell=True)
+
+    except subprocess.CalledProcessError:
+        shutil.copy(backup_file_path, './' + orig)
+
+    os.remove(backup_file_path)
 
 if __name__ == '__main__':
     commands = sys.argv[1]
