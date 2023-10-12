92a1d6867e4cc920bedc65b4d471841f73fde8d5
Tanzir Hasan <63520213+TanzHasan@users.noreply.github.com>
2023-10-12 13:11:06 -0700

    Reorganized the code, it now works for most files, there is still an issue with percpu.h that needs to be resolved but this can slowly break down most headers into smaller versions.

--- auto-iwyu.py
+++ auto-iwyu.py
@@ -6,18 +6,33 @@ import shutil
 
 def main(commands, specific, fixer_path):
     with open (commands) as file:
+        cursed = ["vma.c", "setup.c", "nmi.c"]
         data = json.load(file)
+        dump = []
         for part in data:
-            if part["file"][-len(specific):] != specific: continue
-            perform_iwyu(fixer_path, part)
+            if any([badfile in part["file"] for badfile in cursed]): continue
+            if len(dump) == 5:
+                build_check()
+                while dump:
+                    os.remove(dump.pop())
+            backup = perform_iwyu(fixer_path, part)
+            if backup:
+                dump.append(backup)
 
 def perform_iwyu(fixer_path, part):
     command = part['command'].split()
-    orig = command[-2]
+    orig = command[-1]
     nname = orig[:-1] + 'i'
     backup_file_path = './' + orig[:-2] + '_backup.c'
-    shutil.copy('./'+orig, backup_file_path)
-    subprocess.run(["wc", "-l", f"./{nname}"])
+
+    try:
+        output = subprocess.check_output(f"wc -l ./{nname}", shell=True)
+        old_size = int(output.decode().strip().split()[0])
+    except: #this has already been tried
+        return
+    
+    orig_path = './' + orig
+    shutil.copy(orig_path, backup_file_path)
 
     include = [
         "include-what-you-use",
@@ -26,7 +41,9 @@ def perform_iwyu(fixer_path, part):
         "-Xiwyu",
         "--max_line_length=30",
         "-Xiwyu",
-        "--mapping_file=./filter.imp"
+        "--mapping_file=./filter.imp",
+        "-Xiwyu",
+        "--no_fwd_decls"
     ]
     include_command = include + command[1:] + [f'2>&1 | {fixer_path}']
     # print(include_command)
@@ -34,16 +51,40 @@ def perform_iwyu(fixer_path, part):
 
     command[-2] = nname
     build_command = command + ['-E']
-    
-
+    # print(part)
     try:
-        subprocess.run(' '.join(build_command), shell=True)
+        subprocess.check_call(' '.join(build_command), shell=True)
+        output = subprocess.check_output(f"wc -l ./{nname}", shell=True)
+        new_size = int(output.decode().strip().split()[0])
+        if new_size >= old_size:
+            print(new_size, old_size)
+            raise ValueError
         subprocess.run(f"wc -l ./{nname} >> changes.txt", shell=True)
+        # subprocess.run(f"rm ./{nname}")
+        # subprocess.run("echo ??? >> changes.txt", shell=True)
 
-    except subprocess.CalledProcessError:
-        shutil.copy(backup_file_path, './' + orig)
+    except:
+        shutil.copy(backup_file_path, orig_path)
+        subprocess.run(f"rm ./{nname}", shell=True)
+        subprocess.run(f"rm ./{backup_file_path}", shell=True)
+        subprocess.run(f"echo 'copying {backup_file_path} to {orig_path} due to bugs' >> change_bugs.txt", shell=True)
+        return
 
-    os.remove(backup_file_path)
+    return backup_file_path
+
+def build_check():
+    try:
+        subprocess.check_output("make ARCH=arm LLVM=1 -j 128 defconfig all", shell=True)
+        print("arm works")
+        subprocess.check_output("make LLVM=1 -j 128 defconfig all", shell=True)
+        print("x86 works")
+        # subprocess.check_output("make ARCH=arm64 LLVM=1 -j 128 defconfig all", shell=True)
+        # print("arm64 works")
+        # subprocess.check_output("make ARCH=riscv LLVM=1 -j 128 defconfig all", shell=True)
+        # print("riscv works")
+        
+    except:
+        exit()
 
 if __name__ == '__main__':
     commands = sys.argv[1]
