3442ca829f9ca28478065d2ecadef81b4162f5f3
Tanzir Hasan <tanzirhasanhasan@gmail.com>
2023-11-01 17:38:42 +0000

    raised warnings on improper refactoring for single_iwyu.py

--- single_iwyu.py
+++ single_iwyu.py
@@ -9,12 +9,31 @@ def main(commands, fixer_path, filter, specific):
     filter.append(dir + '/filter.imp')
     filter.append(dir + '/symbol.imp')
 
+    file_used = ""
     with open (commands) as file:
         data = json.load(file)
         for part in data:
             if specific not in part['file']: continue
-            backup = perform_iwyu(fixer_path, part, filters)
+            output = perform_iwyu(fixer_path, part, filters)
+            file_used = part["file"]
             break
+    with open(file_used, 'r') as file:
+        if os.path.exists(file_used[:-1] + 'h'):
+            print("WARNING: HEADER POTENTIALLY MODIFIED")
+        
+        match output:
+            case 0:
+                for line in file:
+                    if 'asm-generic' in line:
+                        print(f'''WARNING: ASM-GENERIC PRESENT IN LINE: {line}
+                                CONSIDER REMOVING''')
+            case 1: 
+                print("WARNING: DOES NOT BUILD")
+            case 2:
+                print("WARNING: NO .i FILE. RUN build_intermediary.py")
+                return
+            case 3:
+                print("WARNING: CHANGES DID NOT LEAD TO REDUCTION IN PREPROCESSING SIZE")
 
 def perform_iwyu(fixer_path, part, filters):
 
@@ -28,7 +47,7 @@ def perform_iwyu(fixer_path, part, filters):
         output = subprocess.check_output(f"wc -l ./{nname}", shell=True)
         old_size = int(output.decode().strip().split()[0])
     except: #this has already been tried
-        return
+        return 2
     
     include = [
         "include-what-you-use",
@@ -59,12 +78,14 @@ def perform_iwyu(fixer_path, part, filters):
         new_size = int(output.decode().strip().split()[0])
         if new_size >= old_size:
             print(new_size, old_size)
-            raise ValueError
+            return 3
         subprocess.run(f"wc -l ./{nname} >> changes.txt", shell=True)
 
     except:
         subprocess.run(f"rm ./{nname}", shell=True)
-        return
+        return 1
+    
+    return 0
 
 
 def build_check():
