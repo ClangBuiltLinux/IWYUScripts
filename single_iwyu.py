8e4b334958a62e5160c57b651b8d00535c2b9dc3
Tanzir Hasan <tanzirhasanhasan@gmail.com>
2023-11-01 19:16:52 +0000

    scanned through commands instead of assuming the location of the fiel

--- single_iwyu.py
+++ single_iwyu.py
@@ -2,6 +2,7 @@ import json
 import subprocess
 import os
 import argparse
+import sys
 from pathlib import Path
 
 def main(commands, fixer_path, filter, specific):
@@ -9,46 +10,30 @@ def main(commands, fixer_path, filter, specific):
     filter.append(dir + '/filter.imp')
     filter.append(dir + '/symbol.imp')
 
-    file_used = ""
     with open (commands) as file:
         data = json.load(file)
         for part in data:
             if specific not in part['file']: continue
-            output = perform_iwyu(fixer_path, part, filters)
-            file_used = part["file"]
+            perform_iwyu(fixer_path, part, filters)
             break
-    with open(file_used, 'r') as file:
-        if os.path.exists(file_used[:-1] + 'h'):
-            print("WARNING: HEADER POTENTIALLY MODIFIED")
-        
-        match output:
-            case 0:
-                for line in file:
-                    if 'asm-generic' in line:
-                        print(f'''WARNING: ASM-GENERIC PRESENT IN LINE: {line}
-                                CONSIDER REMOVING''')
-                build_check()
-            case 1: 
-                print("WARNING: DOES NOT BUILD")
-            case 2:
-                print("WARNING: NO .i FILE. RUN build_intermediary.py")
-                return
-            case 3:
-                print("WARNING: CHANGES DID NOT LEAD TO REDUCTION IN PREPROCESSING SIZE")
 
 def perform_iwyu(fixer_path, part, filters):
 
     command = part['command'].split()
     os.chdir(part["directory"])
 
-    orig = command[-1]
+    for i in range(len(command)):
+        if command[i] == '-o':
+            orig = command[i+1]
+            break
     nname = orig[:-1] + 'i'
 
     try:
         output = subprocess.check_output(f"wc -l ./{nname}", shell=True)
         old_size = int(output.decode().strip().split()[0])
     except: #this has already been tried
-        return 2
+        print("WARNING: NO .i FILE. RUN build_intermediary.py", file=sys.stderr)
+        return 1
     
     include = [
         "include-what-you-use",
@@ -71,21 +56,34 @@ def perform_iwyu(fixer_path, part, filters):
     subprocess.run(' '.join(include_command))
     subprocess.run(' '.join(include_command))
 
+    if os.path.exists(orig[:-1] + 'h'):
+        print("WARNING: HEADER POTENTIALLY MODIFIED", file=sys.stderr)
+
     command[-2] = nname
     build_command = command + ['-E']
     subprocess.check_call(' '.join(build_command), shell=True)
+
     try:
         output = subprocess.check_output(f"wc -l ./{nname}", shell=True)
         new_size = int(output.decode().strip().split()[0])
         if new_size >= old_size:
-            print(new_size, old_size)
-            return 3
+            print("WARNING: CHANGES DID NOT LEAD TO REDUCTION IN PREPROCESSING SIZE", file=sys.stderr)
+            return 1
         subprocess.run(f"wc -l ./{nname} >> changes.txt", shell=True)
 
     except:
         subprocess.run(f"rm ./{nname}", shell=True)
+        print("WARNING: DOES NOT BUILD", file=sys.stderr)
         return 1
-    
+
+    with open(orig, 'r') as file:
+        for line in file:
+            if 'asm-generic' in line:
+                print(f'''WARNING: ASM-GENERIC PRESENT IN LINE: {line}
+                        CONSIDER REMOVING''', file=sys.stderr)
+                
+    if build_check(): return 1
+
     return 0
 
 
@@ -99,10 +97,10 @@ def build_check():
         # print("riscv works")
         subprocess.check_output("make LLVM=1 -j 128 defconfig all", shell=True)
         print("x86 works")
-        
+        return 0
         
     except:
-        exit()
+        return 1
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='''This script attempts to automatically refactor one 
