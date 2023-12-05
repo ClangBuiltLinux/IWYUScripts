7c5c2083efc3c298d3de6ffd0964e39de3ee6cc9
Tanzir Hasan <tanzirhasanhasan@gmail.com>
2023-12-05 18:14:08 +0000

    raise warn on double modification

--- single_iwyu.py
+++ single_iwyu.py
@@ -91,9 +91,12 @@ def perform_iwyu(fixer_path: Path, part: json, filters: List[Path], current_path
     quote_cleaner = ['python', f'{current_path}/lib/remove_quotes.py']
     fix_includes = [f'{fixer_path}', '--noreorder']
 
-    #Runs twice to fix include list once and a second time to catch bad includes
-    if not all(run_cleaned_iwyu(iwyu_cmd, quote_cleaner, fix_includes) for _ in range(2)):
+    if not run_cleaned_iwyu(iwyu_cmd, quote_cleaner, fix_includes):
+        warn("NO CHANGES")
         return False
+
+    if run_cleaned_iwyu(iwyu_cmd, quote_cleaner, fix_includes):
+        warn("X-MACRO OR HEADER MODIFICATION MAY BE PRESENT ")
 
     if outfile.with_suffix('.h').exists():
         warn("HEADER POTENTIALLY MODIFIED")
