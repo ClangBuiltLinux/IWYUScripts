69529dfa9e5c1acbf2b9a0dfcf20e5310f1f4b74
Tanzir Hasan <tanzirhasanhasan@gmail.com>
2023-12-07 21:40:54 +0000

    this stops quotes from being replaced

--- lib/remove_quotes.py
+++ lib/remove_quotes.py
@@ -3,15 +3,22 @@
 import sys
 def replace_quotes_with_angle_brackets(text):
     '''Replaces odd " with < and even " with >'''
-    result = []
     replace_with = '<'
-    for char in text:
-        if char == '"':
-            result.append(replace_with)
-            replace_with = '>' if replace_with == '<' else '<'
+    text = text.split('\n')
+    formatted = []
+    for line in text:
+        if '/' in line and '..' not in line:
+            result = []
+            for char in line:
+                if char == '"':
+                    result.append(replace_with)
+                    replace_with = '>' if replace_with == '<' else '<'
+                else:
+                    result.append(char)
+            formatted.append(''.join(result))
         else:
-            result.append(char)
-    return ''.join(result)
+            formatted.append(line)
+    return '\n'.join(formatted)
 
 if __name__ == "__main__":
     print(replace_quotes_with_angle_brackets(sys.stdin.read()))
