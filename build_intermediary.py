70d953b43368a5697b3f45ef592fec5edf91bf4a
Tanzir Hasan <tanzirhasanhasan@gmail.com>
2023-12-13 18:19:01 +0000

    Infers arg type from extension

--- build_intermediary.py
+++ build_intermediary.py
@@ -50,8 +50,12 @@ if __name__ == '__main__':
     parser.add_argument('-e', '--extension', type=str,
                         help='File extension type .i, .ll, .o etc',
                         default='.i')
-    parser.add_argument('-a', '--additional_arg', type=str,
-                        help='Any additional arg necessary to build extension type',
-                        default='-E')
+
     args = parser.parse_args()
-    main(args.commands, args.threads, args.extension, args.additional_arg)
+    arg_mapping = {
+        '.i': '-E',
+        '.s': '-S',
+        '.bc': '-emit-llvm',
+        '.ll': '-emit-llvm -S',
+    }
+    main(args.commands, args.threads, args.extension, arg_mapping[args.extension])
